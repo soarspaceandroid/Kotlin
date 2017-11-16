8f71c32b0b594f95a9444773ff0574d99debaf52
gaofei <gaofei@yonglibao.com>
2017-11-16 16:29:48 +0800

    add project

---
+++ app/src/main/java/ui/android/yonglibao/com/kotlinapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package ui.android.yonglibao.com.kotlinapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        requestButton.setOnClickListener(View.OnClickListener {
+            //请求
+        })
+
+    }
+}
