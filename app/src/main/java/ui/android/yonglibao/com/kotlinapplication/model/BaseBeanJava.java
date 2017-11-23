d1f44c7bb51f6cbda1b077c437006a170a94a63e
gaofei <gaofei@yonglibao.com>
2017-11-23 18:17:35 +0800

    修改

---
+++ app/src/main/java/ui/android/yonglibao/com/kotlinapplication/model/BaseBeanJava.java
@@ -0,0 +1,19 @@
+package ui.android.yonglibao.com.kotlinapplication.model;
+
+/**
+ * ----------------------------------------------------
+ * ※ Author :  GaoFei
+ * ※ Date : 2017/11/23
+ * ※ Time : 18:07
+ * ※ Project : Kotlin
+ * ※ Package : ui.android.yonglibao.com.kotlinapplication.model
+ * ----------------------------------------------------
+ */
+
+public class BaseBeanJava {
+
+    public String nu ;
+    public String message;
+    public String com;
+
+}
---
+++ app/src/main/java/ui/android/yonglibao/com/kotlinapplication/model/InfoJava.java
@@ -0,0 +1,17 @@
+package ui.android.yonglibao.com.kotlinapplication.model;
+
+/**
+ * ----------------------------------------------------
+ * ※ Author :  GaoFei
+ * ※ Date : 2017/11/23
+ * ※ Time : 18:07
+ * ※ Project : Kotlin
+ * ※ Package : ui.android.yonglibao.com.kotlinapplication.model
+ * ----------------------------------------------------
+ */
+
+public class InfoJava extends BaseBeanJava {
+
+
+
+}
